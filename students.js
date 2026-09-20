e80e357131e3c88db2bb3084aa8691ec1fe67e63
duyban86-svg <duyban86@gmail.com>
2026-09-21 06:03:59 +0700

    Add files via upload

---
+++ students.js
@@ -0,0 +1,598 @@
+﻿const STUDENTS_LIST = [
+    {
+        "stt":  1,
+        "name":  "Đồng Hà Anh"
+    },
+    {
+        "stt":  2,
+        "name":  "Trần Hoàng Minh Anh"
+    },
+    {
+        "stt":  3,
+        "name":  "Trần Lê Tuấn Anh"
+    },
+    {
+        "stt":  4,
+        "name":  "Trần Nam Anh"
+    },
+    {
+        "stt":  5,
+        "name":  "Nguyễn Quỳnh Anh"
+    },
+    {
+        "stt":  6,
+        "name":  "Nguyễn Đắc Thiên Bảo"
+    },
+    {
+        "stt":  7,
+        "name":  "Nguyễn Chí Cường"
+    },
+    {
+        "stt":  8,
+        "name":  "Nguyễn Quốc Danh"
+    },
+    {
+        "stt":  9,
+        "name":  "Ngô Quang Duy"
+    },
+    {
+        "stt":  10,
+        "name":  "Lý Vĩnh Đạt"
+    },
+    {
+        "stt":  11,
+        "name":  "Cao Hoàng Bảo Hân"
+    },
+    {
+        "stt":  12,
+        "name":  "Trần Thị Mỹ Hân"
+    },
+    {
+        "stt":  13,
+        "name":  "Nguyễn Thúy Hiền"
+    },
+    {
+        "stt":  14,
+        "name":  "Nguyễn Minh Hiếu"
+    },
+    {
+        "stt":  15,
+        "name":  "Nguyễn Thiện Hiếu"
+    },
+    {
+        "stt":  16,
+        "name":  "Trần Thái Hòa"
+    },
+    {
+        "stt":  17,
+        "name":  "Trần Thị Khánh Huyền"
+    },
+    {
+        "stt":  18,
+        "name":  "Nguyễn Ngọc Quỳnh Hương"
+    },
+    {
+        "stt":  19,
+        "name":  "Lê Cao Minh Khôi"
+    },
+    {
+        "stt":  20,
+        "name":  "Trương Hoàng Khôi"
+    },
+    {
+        "stt":  21,
+        "name":  "Lâm Ngọc Lan"
+    },
+    {
+        "stt":  22,
+        "name":  "Lữ Cát Gia Lượng"
+    },
+    {
+        "stt":  23,
+        "name":  "Nguyễn Minh Mẫn"
+    },
+    {
+        "stt":  24,
+        "name":  "Nguyễn Xuân Minh"
+    },
+    {
+        "stt":  25,
+        "name":  "Phạm Thị Trà My"
+    },
+    {
+        "stt":  26,
+        "name":  "Nguyễn Vũ Hoàng Nam"
+    },
+    {
+        "stt":  27,
+        "name":  "Nguyễn Phạm Thu Ngân"
+    },
+    {
+        "stt":  28,
+        "name":  "Đỗ Lê Phương Nghi"
+    },
+    {
+        "stt":  29,
+        "name":  "Nguyễn Vương Yến Nhi"
+    },
+    {
+        "stt":  30,
+        "name":  "Uông Mai Thanh Phong"
+    },
+    {
+        "stt":  31,
+        "name":  "Ngụy Gia Phúc"
+    },
+    {
+        "stt":  32,
+        "name":  "Đặng Ngọc Ánh Phương"
+    },
+    {
+        "stt":  33,
+        "name":  "Chung Nguyễn Hồng Phương"
+    },
+    {
+        "stt":  34,
+        "name":  "Dương Xuân Hoàng Quân"
+    },
+    {
+        "stt":  35,
+        "name":  "Trương Bùi Giàu Sang"
+    },
+    {
+        "stt":  36,
+        "name":  "Nguyễn Thị Thanh Tâm"
+    },
+    {
+        "stt":  37,
+        "name":  "Phan Thị Thanh Tâm"
+    },
+    {
+        "stt":  38,
+        "name":  "Lê Hoàng Thi Thơ"
+    },
+    {
+        "stt":  39,
+        "name":  "Nguyễn Trần Vĩnh Thụy"
+    },
+    {
+        "stt":  40,
+        "name":  "Nguyễn Ngọc Minh Thư"
+    },
+    {
+        "stt":  41,
+        "name":  "Lê Nguyễn Minh Thương"
+    },
+    {
+        "stt":  42,
+        "name":  "Lê Nguyễn Hoàng Thy"
+    },
+    {
+        "stt":  43,
+        "name":  "Lê Vũ Đức Tiến"
+    },
+    {
+        "stt":  44,
+        "name":  "Nguyễn Thiện Toàn"
+    },
+    {
+        "stt":  45,
+        "name":  "Đỗ Võ Minh Tới"
+    },
+    {
+        "stt":  46,
+        "name":  "Nguyễn Ngọc Quỳnh Trâm"
+    },
+    {
+        "stt":  47,
+        "name":  "Bùi Quốc Trung"
+    },
+    {
+        "stt":  48,
+        "name":  "Vũ Minh Thanh Tùng"
+    },
+    {
+        "stt":  49,
+        "name":  "Phan Quang Vinh"
+    },
+    {
+        "stt":  50,
+        "name":  "Lê Ngọc Tường Vy"
+    },
+    {
+        "stt":  51,
+        "name":  "Trần Nguyễn Tường Vy"
+    },
+    {
+        "stt":  52,
+        "name":  "Lê Thị Qúy Vy"
+    },
+    {
+        "stt":  1,
+        "name":  "Đặng Hà Anh"
+    },
+    {
+        "stt":  2,
+        "name":  "Đặng Hải Anh"
+    },
+    {
+        "stt":  3,
+        "name":  "Mai Phương Anh"
+    },
+    {
+        "stt":  4,
+        "name":  "Lê Tuấn Anh"
+    },
+    {
+        "stt":  5,
+        "name":  "Lê Tùng Anh"
+    },
+    {
+        "stt":  6,
+        "name":  "Nguyễn Hoàng Bách"
+    },
+    {
+        "stt":  7,
+        "name":  "Huỳnh Phúc Gia Bảo"
+    },
+    {
+        "stt":  8,
+        "name":  "Nguyễn Quốc Bảo"
+    },
+    {
+        "stt":  9,
+        "name":  "Nhữ Đình Quốc Bình"
+    },
+    {
+        "stt":  10,
+        "name":  "Trần Ngọc Thùy Dương"
+    },
+    {
+        "stt":  11,
+        "name":  "Nguyễn Hải Đăng"
+    },
+    {
+        "stt":  12,
+        "name":  "Trần Trung Hiếu"
+    },
+    {
+        "stt":  13,
+        "name":  "Lê Gia Huy"
+    },
+    {
+        "stt":  14,
+        "name":  "Đào Hoàng Gia Huy"
+    },
+    {
+        "stt":  15,
+        "name":  "Lê Phạm Minh Huy"
+    },
+    {
+        "stt":  16,
+        "name":  "Huỳnh Tuấn Hưng"
+    },
+    {
+        "stt":  17,
+        "name":  "Phạm Gia Khang"
+    },
+    {
+        "stt":  18,
+        "name":  "Nguyễn Trần Hoàng Khang"
+    },
+    {
+        "stt":  19,
+        "name":  "Trần Ngô Tuấn Khoa"
+    },
+    {
+        "stt":  20,
+        "name":  "Trần Thiện Khoa"
+    },
+    {
+        "stt":  21,
+        "name":  "Nguyễn Lâm Khương"
+    },
+    {
+        "stt":  22,
+        "name":  "Nguyễn Phan Trung Kiên"
+    },
+    {
+        "stt":  23,
+        "name":  "Nguyễn Ngọc Hải Lam"
+    },
+    {
+        "stt":  24,
+        "name":  "Nguyễn Trắc Gia Mẫn"
+    },
+    {
+        "stt":  25,
+        "name":  "Đoàn Anh Minh"
+    },
+    {
+        "stt":  26,
+        "name":  "Đoàn Hoàng Minh"
+    },
+    {
+        "stt":  27,
+        "name":  "Hoàng Thiên Minh"
+    },
+    {
+        "stt":  28,
+        "name":  "Huỳnh Trần Anh Minh"
+    },
+    {
+        "stt":  29,
+        "name":  "Nguyễn Lê Phương Nam"
+    },
+    {
+        "stt":  30,
+        "name":  "Lê Nguyễn Quốc Nam"
+    },
+    {
+        "stt":  31,
+        "name":  "Nguyễn Đoàn Khánh Ngọc"
+    },
+    {
+        "stt":  32,
+        "name":  "Nguyễn Hữu Phúc"
+    },
+    {
+        "stt":  33,
+        "name":  "Nguyễn Minh Quân"
+    },
+    {
+        "stt":  34,
+        "name":  "Hoàng Vương Quân"
+    },
+    {
+        "stt":  35,
+        "name":  "Nguyễn Trường Sơn"
+    },
+    {
+        "stt":  36,
+        "name":  "Lê Nguyên Thịnh"
+    },
+    {
+        "stt":  37,
+        "name":  "Lại Thái Thịnh"
+    },
+    {
+        "stt":  38,
+        "name":  "Hà Minh Thư"
+    },
+    {
+        "stt":  39,
+        "name":  "Nguyễn Ngọc Minh Thư"
+    },
+    {
+        "stt":  40,
+        "name":  "Trần Nhật Trường"
+    },
+    {
+        "stt":  41,
+        "name":  "Nguyễn Ngọc Anh Tuấn"
+    },
+    {
+        "stt":  42,
+        "name":  "Nguyễn Ngọc Phương Vi"
+    },
+    {
+        "stt":  43,
+        "name":  "Võ Quốc Vinh"
+    },
+    {
+        "stt":  44,
+        "name":  "Nguyễn Thành Vinh"
+    },
+    {
+        "stt":  45,
+        "name":  "Phan Ngọc Phương Vy"
+    },
+    {
+        "stt":  46,
+        "name":  "Nguyễn Vũ Lan Vy"
+    },
+    {
+        "stt":  1,
+        "name":  "Lâm Tuyết An"
+    },
+    {
+        "stt":  2,
+        "name":  "Lê Bá Trâm Anh"
+    },
+    {
+        "stt":  3,
+        "name":  "Nguyễn Ngọc Vy Anh"
+    },
+    {
+        "stt":  4,
+        "name":  "Phan Vân Anh"
+    },
+    {
+        "stt":  5,
+        "name":  "Nguyễn Thị Ngọc Ánh"
+    },
+    {
+        "stt":  6,
+        "name":  "Nguyễn Thị Thanh Bình"
+    },
+    {
+        "stt":  7,
+        "name":  "Nguyễn Hoàng Mỹ Diệu"
+    },
+    {
+        "stt":  8,
+        "name":  "Lai Vũ Minh Duy"
+    },
+    {
+        "stt":  9,
+        "name":  "Trần Nguyễn Ngân Hà"
+    },
+    {
+        "stt":  10,
+        "name":  "Hà Thanh Hà"
+    },
+    {
+        "stt":  11,
+        "name":  "Trần Thị Thu Hà"
+    },
+    {
+        "stt":  12,
+        "name":  "Đinh Nguyễn Quang Hải"
+    },
+    {
+        "stt":  13,
+        "name":  "Lâm Gia Hân"
+    },
+    {
+        "stt":  14,
+        "name":  "Nguyễn Ngọc Gia Hân"
+    },
+    {
+        "stt":  15,
+        "name":  "Nguyễn Hà Diệu Hiền"
+    },
+    {
+        "stt":  16,
+        "name":  "Huỳnh Xuân Hồng"
+    },
+    {
+        "stt":  17,
+        "name":  "Lê Hùng Huy"
+    },
+    {
+        "stt":  18,
+        "name":  "Huỳnh An Khang"
+    },
+    {
+        "stt":  19,
+        "name":  "Nguyễn Nam Khánh"
+    },
+    {
+        "stt":  20,
+        "name":  "Phùng Anh Khoa"
+    },
+    {
+        "stt":  21,
+        "name":  "Võ Đăng Khôi"
+    },
+    {
+        "stt":  22,
+        "name":  "Đỗ Huỳnh Đăng Khôi"
+    },
+    {
+        "stt":  23,
+        "name":  "Phạm Thị Thiên Kim"
+    },
+    {
+        "stt":  24,
+        "name":  "Huỳnh Hải Lam"
+    },
+    {
+        "stt":  25,
+        "name":  "Ngô Đinh Nhật Lan"
+    },
+    {
+        "stt":  26,
+        "name":  "Nguyễn Hoàng Lân"
+    },
+    {
+        "stt":  27,
+        "name":  "Trần Khánh Linh"
+    },
+    {
+        "stt":  28,
+        "name":  "Lê Nguyễn Kim Lộc"
+    },
+    {
+        "stt":  29,
+        "name":  "Nguyễn Thành Lộc"
+    },
+    {
+        "stt":  30,
+        "name":  "Nguyễn Dương Minh"
+    },
+    {
+        "stt":  31,
+        "name":  "Hoàng Lê Hồng Minh"
+    },
+    {
+        "stt":  32,
+        "name":  "Tuấn Nguyên Nhật Minh"
+    },
+    {
+        "stt":  33,
+        "name":  "Nguyễn Trần Nhật Minh"
+    },
+    {
+        "stt":  34,
+        "name":  "Đinh Thảo My"
+    },
+    {
+        "stt":  35,
+        "name":  "Bùi Nhất Nam"
+    },
+    {
+        "stt":  36,
+        "name":  "Trang Hà Thanh Ngân"
+    },
+    {
+        "stt":  37,
+        "name":  "Trần Khánh Ngọc"
+    },
+    {
+        "stt":  38,
+        "name":  "Trần Ngọc Thảo Nhi"
+    },
+    {
+        "stt":  39,
+        "name":  "Trần Ngọc Yến Nhi"
+    },
+    {
+        "stt":  40,
+        "name":  "Khổng Vũ Quỳnh Như"
+    },
+    {
+        "stt":  41,
+        "name":  "Bùi Đặng Trúc Phương"
+    },
+    {
+        "stt":  42,
+        "name":  "Vương Đình Quốc Thái"
+    },
+    {
+        "stt":  43,
+        "name":  "Phạm Lan Thảo"
+    },
+    {
+        "stt":  44,
+        "name":  "Huỳnh Thị Xuân Thảo"
+    },
+    {
+        "stt":  45,
+        "name":  "Vũ Lê Bảo Trân"
+    },
+    {
+        "stt":  46,
+        "name":  "Bùi Ngọc Bảo Trân"
+    },
+    {
+        "stt":  47,
+        "name":  "Võ Phạm Khánh Trình"
+    },
+    {
+        "stt":  48,
+        "name":  "Phạm Trần Mỹ Trúc"
+    },
+    {
+        "stt":  49,
+        "name":  "Nguyễn Ngọc Cẩm Tú"
+    },
+    {
+        "stt":  50,
+        "name":  "Lê Phan Thanh Vân"
+    },
+    {
+        "stt":  51,
+        "name":  "Nguyễn Thụy Tường Vy"
+    }
+];
